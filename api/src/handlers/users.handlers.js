bcf3bb1808650ac26b1e6cd752b66a81632ebe07
unknown <tazza.personal@gmail.com>
2023-03-29 15:32:48 -0300

    prueba1

--- api/src/handlers/users.handlers.js
+++ api/src/handlers/users.handlers.js
@@ -4,7 +4,7 @@ const createUser = async (req, res) => {
     const { firstname, lastname, email, mobile, password, role, nationality, status } = req.body;
     try {
         const user = await newUser(firstname, lastname, email, mobile, password, role, nationality, status);
-        res.status(201).send({userId: user.id});
+        res.status(201).send(user);
 
     } catch (error) {
         res.status(400).json({ message: 'Error in user creation', error: error});
