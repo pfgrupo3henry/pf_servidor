9cae5186a91b31c590a77af5c1fbcb5fe4203ae7
RocioAlday <roo.alday@gmail.com>
2023-03-29 19:35:45 -0300

    Add features: Login, Auth & GetUsersRoute

---
+++ api/src/middlewares/adminMiddleware.js
@@ -0,0 +1,16 @@
+const { User }= require ('../db');
+
+const isAdmin= async(req, res, next)=>{
+    console.log(req.user);
+    const {email}= req.user;
+    const adminUser= await User.findOne({where: {email: email}});
+    if (adminUser.role !== "admin") {
+        throw new Error('You are not admin');
+    } else{
+        next();
+    }
+};
+
+module.exports= {
+    isAdmin,
+}
---
+++ api/src/utils/data-users.js
@@ -0,0 +1,27 @@
+const users= [
+    {
+        firstname: 'Rocio',
+        lastname: 'Alday',
+        email: 'rocioaldauy89@hotmail.com',
+        password: 'pepito789',
+        nationality: 'argentina'
+    },
+    {
+        firstname: 'Lucas',
+        lastname: 'Moreno',
+        email: 'lucasM56@hotmail.com',
+        password: 'luqitas36',
+        nationality: 'argentina'
+    },
+    {
+        firstname: 'Micaela',
+        lastname: 'Lopez',
+        email: 'micalopez@hotmail.com',
+        password: 'micakl58',
+        nationality: 'argentina',
+        role: 'Admin'
+    }
+
+];
+
+module.exports = users;
