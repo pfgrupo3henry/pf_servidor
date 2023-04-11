1835bec4fbdedf5da4fb6bdc1374c2a269674edb
unknown <tazza.personal@gmail.com>
2023-04-11 00:37:50 -0300

    payment

--- api/src/controllers/mercadoPago.controllers.js
+++ api/src/controllers/mercadoPago.controllers.js
@@ -1,4 +1,5 @@
 const mercadopago = require("mercadopago");
+const { User, Review, Payment, Order } = require('../db');
 const { Videogame } = require('../models/Videogame');
 const { query } = require("express");
 const product = Videogame;
@@ -6,11 +7,19 @@ mercadopago.configure({
   access_token: process.env.ACCESS_TOKEN,
 });
 
+const succesPayment = async (req,res) => {
+
+
+
+};
+
+
 const paymentPostController = async (req, res) => {
 
  
   const prod = req.body;
   const totalPrice = req.body.totalPrice;
+  const userId = req.body.userId;
  
   try {
     const preference = {
@@ -28,22 +37,22 @@ const paymentPostController = async (req, res) => {
       ],
       back_urls: {
         //rutas de acuerdo a como haya salido la transacion
-        success: 'https://pf-front-y72g-git-develop-pfgrupo3henry.vercel.app/home',
-        failure: '',
+        success: 'http://localhost:3000/home',
+        failure: 'http://localhost:3000/home',
         pending: '',
       },
-      notification_url: 'https://00f1-186-138-114-149.sa.ngrok.io/payment',
+      auto_return: "approved",
+      notification_url: 'https://405f-2800-810-451-668-48ce-17a0-ccde-af9c.ngrok-free.app/payment',
       statement_descriptor: "Henry Game Store",
-      auto_return: 'approved',
       // para que no se puedan hacer pagos pendientes (rapipago, etc)
       binary_mode: true,
     };
 
-    return await mercadopago.preferences.create(preference).then((response) => {
+     // 1. Crear la preferencia de pago
+     return await mercadopago.preferences.create(preference).then((response) => {
 
       res.status(200).send({ response: response });
-      
-    });
+  })
   } catch (error) {
     res.status(400).json({error: error.message})
   }
@@ -51,11 +60,17 @@ const paymentPostController = async (req, res) => {
 
 // para recibir la info del pago
 const getPaymentInfo = async (req, res, next) => {
+  const {query} = req
+  let hola = req.query[data.id]
+  let pay = req.query.type
+  console.log("holaaaaaaaaaaa", hola, pay)
+  const topic = query.topic
   // const payment_id = req.query.payment_id;
   // const payment_status = req.query.status;
   // const external_reference = req.query.external_reference;
 
-  const merchantOrder = null;
+  
+var merchantOrder;  
 
 switch (topic) {
   case "payment":
@@ -66,12 +81,11 @@ switch (topic) {
     //   })
     const paymentId = query.id
     const payment = await mercadopago.payment.findById(paymentId);
+    console.log("ESTATUS",payment.response.status)
     merchantOrder = await mercadopago.merchant_orders.findById(payment.body.order.id)
-    .catch((error) => {
-        console.log(error);
-      });
+    await Payment.create({info: payment})
+    
     break;
-    console.log(payment);
   case "merchant_order":
     // mercadopago.merchant_orders.get(req.query.id).then((order) => {
     //   merchant_order = order;
@@ -79,12 +93,11 @@ switch (topic) {
     // })
     const orderId = query.id;
     merchantOrder = await mercadopago.merchant_orders.findById(orderId)
-    .catch((error) => {
-      console.log(error);
-    });
-    break;
+    console.log(" este merchantOrder", merchantOrder)
+    
+  break;
 }
-console.log(merchantOrder.body.payments);
+/* console.log(merchantOrder.body.payments);
 
   const paidAmount = 0;
   merchantOrder.body.payments.forEach(payment => {
@@ -95,7 +108,9 @@ console.log(merchantOrder.body.payments);
     } else {
     console.log("No se pudo realizar el pago");
   }
-  });
+  }); */
+
+  res.status(200).send()
 }
 
 
