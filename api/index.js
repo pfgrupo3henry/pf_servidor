697e1ad73bfa7dea26900be24f0c9d7d108fca23
Javier <52515632+javlucky@users.noreply.github.com>
2023-04-20 10:22:43 -0300

    feat reset password

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 
-conn.sync({force: true}).then(() => {
+conn.sync({alter: true}).then(() => {
 
   server.listen(port, () => {
     console.log(`Server raised in port ${port}`); // eslint-disable-line no-console
