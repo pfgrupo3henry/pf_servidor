b6a71d48f8b3b59fc3e5c4d6778b0ad86c3c9525
Javier <52515632+javlucky@users.noreply.github.com>
2023-04-20 19:21:51 -0300

    Update users.handlers.js

--- api/src/handlers/users.handlers.js
+++ api/src/handlers/users.handlers.js
@@ -185,7 +185,7 @@ const createNewPassword = async (req, res) => {
       });
         res.status(200).json(user);
       } catch (error) {
-        return res.status(400).json({message: error});
+        return res.status(400).json({message: error.message});
       }
 }
 
