bc029bc4ffd224ae9dbd89c9075070a98564f833
unknown <tazza.personal@gmail.com>
2023-04-20 14:04:10 -0300

    route to modify img of games

--- api/src/handlers/videogames.handlers.js
+++ api/src/handlers/videogames.handlers.js
@@ -124,8 +124,100 @@ const modifyVideogameHandler = async (req, res) => {
   }
 };
 
+const modifyGameWithImg = async (req, res) => {
+  const{ name, description, img, platform, genre, price, stock } = req.body;
+
+  try {
+
+    if(img.length > 0){
+      // Generate The output url    
+      let id = newName || name
+      const res = await cloudinary.uploader.upload(`${img[0]}`, {folder: "img_new_game", public_id: `newGame-${id}`})
+
+      img[0] = res.url
+  }
+
+    const actualizado = await Videogame.update({
+      description: description, img: img, price: price, stock: stock
+    },{
+      where: { name: name }
+    });
+  
+    let videogame = await Videogame.findOne({
+      where: { name: name },
+      include: [
+        {
+          model: Platform,
+          attributes: ['name']
+        },
+        {
+          model: Genre,
+          attributes: ['name']
+        }
+      ],
+    });
+
+    if(!videogame) {
+      throw "The videogame with the name selected isn't available" 
+    }
+
+
+    if(platform) {
+
+    const platformDb = await Platform.findOne({where: {name: platform}})
+
+    await videogame.setPlatforms([]);
+
+    await videogame.addPlatform(platformDb, { through: { status: platform } });
+
+    }
+
+
+    if(genre) {
+
+      const genreDb = await Genre.findOne({where: {name: genre}})
+  
+      await videogame.setGenres([]);
+  
+      await videogame.addGenre(genreDb, { through: { status: genre} });
+  
+      }
+
+      videogame = await Videogame.findOne({
+        where: { name: name },
+        include: [
+          {
+            model: Platform,
+            attributes: ['name']
+          },
+          {
+            model: Genre,
+            attributes: ['name']
+          }
+        ],
+      });
+ 
+    videogame= {
+      id: videogame.id,
+      name: videogame.name, 
+      description: videogame.description,
+      img: videogame.img,
+      price: videogame.price,
+      genre: videogame.genres[0].name,
+      platform: videogame.platforms[0].name,
+      stock: videogame.stock
+      } 
+
+    res.status(201).json(videogame);
+
+  } catch(error) {
+      res.status(400).json({ error: "Error al modificar", message: error });
+  }
+};
+
 module.exports = { 
   getVideogames,  
   modifyVideogameHandler,
+  modifyGameWithImg,
   getVideogameById
 };
